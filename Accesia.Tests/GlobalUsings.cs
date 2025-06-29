0a7d59858d01e7fc80ff12315bc95eb5fb7500ec
Nestor Gutierrez <nestorg456k@outlook.com>
2025-06-29 00:04:20 -0500

    feat: agregar proyecto de pruebas y configuraciones necesarias
    - Se creó el archivo Accesia.Tests.csproj con las dependencias requeridas.
    - Se añadió el archivo GlobalUsings.cs para importar los espacios de nombres necesarios.
    - Se actualizó la solución para incluir el nuevo proyecto de pruebas.

---
+++ Accesia.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using FluentAssertions;
+
+// Domain imports
+global using Accesia.Domain.Entities;
+global using Accesia.Domain.ValueObjects;
+global using Accesia.Domain.Enums;
+global using Accesia.Domain.Common;
